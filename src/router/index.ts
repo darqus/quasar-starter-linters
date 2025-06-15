4ef7514de25d1c7198952c9c13659934a5ee65bd
darqus <voladores@inbox.ru>
2025-06-16 02:28:55 +0300

    ref router

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,24 +18,17 @@ import routes from './routes'
  * with the Router instance.
  */
 
-export default defineRouter(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : process.env.VUE_ROUTER_MODE === 'history'
-      ? createWebHistory
-      : createWebHashHistory
+// Create history based on environment
+const createHistory = process.env.SERVER
+  ? createMemoryHistory
+  : process.env.VUE_ROUTER_MODE === 'history'
+    ? createWebHistory
+    : createWebHashHistory
 
-  const Router = createRouter({
-    scrollBehavior: () => ({
-      left: 0, top: 0,
-    }),
-    routes,
-
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE),
-  })
-
-  return Router
-})
+export default defineRouter(() => createRouter({
+  scrollBehavior: () => ({
+    left: 0, top: 0, 
+  }),
+  routes,
+  history: createHistory(process.env.VUE_ROUTER_BASE),
+}))
